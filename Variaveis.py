96ee5cc3120e17463d3c363e474a2b3f5c987e3a
Paulo-Fernandodev <paulofernandosantos19@hmail.com>
2026-08-11 18:18:31 -0300

    Projeto caixa eletrônico

--- Variaveis.py
+++
@@ -1,6 +0,0 @@
-nome = "Fernando"
-idade = 24
-altura = 1.78
-dev = True
-
-print(f"Olá, {nome}! Você tem {idade} anos e mede {altura}m.")
--- recap.py
+++
@@ -1,5 +0,0 @@
-palavra = "nando"
-letraprocurada = "o"
-for letra in palavra:
-    if letra == letraprocurada:
-        print ("Essa palavra tem a letra!", letraprocurada)
